9cce6633c43468f28abb0aa5e6199d5c291a1e9f
yhs0120 <112959032+yhs0120@users.noreply.github.com>
2023-01-10 21:35:37 +0900

    Project_graduate

---
+++ Project_graduate/BookCoverCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookCoverCollectionViewCell.swift
+//  Project_graduate
+//
+//  Created by 윤한솔 on 2023/01/04.
+//
+
+import UIKit
+
+class BookCoverCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var BookCoverImage: UIImageView!
+}
